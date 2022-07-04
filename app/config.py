4763e7f752e67474cc9f02b102317521d7f8ed7a
Kirill Smotrov <kirya.smotrov.02@mail.ru>
2022-07-04 18:42:03 +0300

    main

--- app/config.py
+++ app/config.py
@@ -3,9 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from os import getenv
+from os import getenv, path
 
 
+basedir = path.abspath(path.dirname(__file__))
 db = SQLAlchemy()
 login_manager = LoginManager()
 migrate = Migrate()
@@ -14,8 +15,8 @@ admin = Admin()
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/blog_db'
+    app.secret_key = getenv("SECRET_KEY")
+    app.config['SQLALCHEMY_DATABASE_URI'] = getenv("DATABASE_URL", "sqlite://")
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     login_manager.init_app(app)
