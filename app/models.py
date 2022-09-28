323b706f9b86dbda9003c230e702f8ae32b06f33
Kirill <87494520+spacefellow@users.noreply.github.com>
2022-09-28 21:15:03 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -8,7 +8,7 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    pwd = db.Column(db.String, nullable=False, unique=True)
+    pwd = db.Column(db.String, unique=True, nullable=False)
     posts = db.relationship('Post', cascade="all, delete-orphan", backref='user', lazy='dynamic')
 
     def __repr__(self):
@@ -18,9 +18,9 @@ class User(UserMixin, db.Model):
 class Post(db.Model):
     __tablename__ = 'post'
     post_id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    intro = db.Column(db.String(300))
-    text = db.Column(db.Text)
+    title = db.Column(db.String(100), nullable=False)
+    intro = db.Column(db.String(300), nullable=False)
+    text = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.utcnow)
     user_name = db.Column(db.String(80), db.ForeignKey('user.username'))
 
